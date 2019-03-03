b865cfff631443f290fd2a83dc9bc3bcaaac671d
Marissa Douglass <mahdouglass@gmail.com>
2019-03-02 20:55:12 -0500

    Removed additional div for gif-buttons and added id for gif section. Added js for giphy api.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
-const topics = ['funny', 'sad', 'angry', 'emotions'];
+let topics = ['Leslie Knope', 'Liz Lemon', 'April Ludgate', 'Nick Offerman', 'Michael Scott'];
 
+// Run through topics array and create a button for each element
 for(var i = 0; i < topics.length; i++) {
     var button = document.createElement("button");
     var text = document.createTextNode(topics[i]);
@@ -7,3 +8,10 @@ for(var i = 0; i < topics.length; i++) {
     var element = document.getElementById("gif-buttons")
     element.appendChild(button);
 }
+
+var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=ICPdxSFFdZlkvUdwAnEaJyosRmdukebU&limit=5");
+xhr.done(
+    function(data) { 
+        console.log("success got data", data); 
+    }
+);
