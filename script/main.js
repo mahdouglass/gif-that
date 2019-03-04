94cb4d8961a6f1a2d665a283b28e8150cb28ad9b
Marissa Douglass <mahdouglass@gmail.com>
2019-03-03 22:20:00 -0500

    Looping through results and creating html container and image, passing data image to element.

--- script/main.js
+++ script/main.js
@@ -8,10 +8,33 @@ for(var i = 0; i < topics.length; i++) {
     var element = document.getElementById("gif-buttons")
     element.appendChild(button);
 }
+
 var apiKey = "ICPdxSFFdZlkvUdwAnEaJyosRmdukebU";
 var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + apiKey + "&limit=10";
 
 $.ajax({
     url: queryUrl,
     method: "GET"
-})
+}).then(function(result) {
+    let {data, meta} = result;
+
+    // Loop through each result item //
+    for (var i = 0; i < result.data.length; i++) {
+            
+        // Create a div to hold the sitcom //
+        var gifContainer = $("<div class='gif'>");
+        
+        // Create an element to hold the image //
+        var gifImage = $("<img class='image'>");
+
+        let results = result.data;
+
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+
+        // Display the image //
+        gifContainer.append(gifImage);
+
+        // Putting the entire sitcom above the previous sitcoms //
+        $("#display-gifs").prepend(gifContainer);
+    }
+});
