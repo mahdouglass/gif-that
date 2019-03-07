1bd4bffd2f880760afd255c6b5fce49e7773a6b9
Marissa Douglass <mahdouglass@gmail.com>
2019-03-06 22:42:23 -0500

    Moved button for loop into a function to re-use it when I need to add more buttons to the array.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let topics = ['Leslie Knope', 'Liz Lemon', 'April Ludgate', 'Nick Offerman', 'Michael Scott'];
 
 // Run through topics array and create a button for each element
-for(var i = 0; i < topics.length; i++) {
-    var button = document.createElement("button");
-    var text = document.createTextNode(topics[i]);
-    button.appendChild(text);
-    var element = document.getElementById("gif-buttons")
-    element.appendChild(button);
+function topicsButtons() {
+    for(var i = 0; i < topics.length; i++) {
+        var button = document.createElement("button");
+        var text = document.createTextNode(topics[i]);
+        button.appendChild(text);
+        var element = document.getElementById("gif-buttons")
+        element.appendChild(button);
+    }
 }
 
 var apiKey = "ICPdxSFFdZlkvUdwAnEaJyosRmdukebU";
@@ -43,3 +45,6 @@ $.ajax({
         $("#display-gifs").prepend(gifContainer);
     }
 });
+
+//Call topicsButtons function to populate buttons
+topicsButtons();
