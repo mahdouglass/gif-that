6d0bde5fdae556ac7705b81bbde3a02fb6301db4
Marissa Douglass <mahdouglass@gmail.com>
2019-03-10 21:41:43 -0400

    Wrapped api get data into a function and it gets called when you click on a topic array. Not updating the gifs to the right topic yet.

--- js/main.js
+++ js/main.js
@@ -12,42 +12,44 @@ function topicsButtons() {
     }
 }
 
-// Connect to api and get data
-var apiKey = "ICPdxSFFdZlkvUdwAnEaJyosRmdukebU";
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + apiKey + "&limit=10";
+function displayGifs() {
+    // Connect to api and get data
+    var apiKey = "ICPdxSFFdZlkvUdwAnEaJyosRmdukebU";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + apiKey + "&limit=10";
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(result) {
-    let data = result;
-    console.log(result);
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(result) {
+        let data = result;
+        console.log(result);
 
-    // Loop through each result item //
-    for (var i = 0; i < result.data.length; i++) {
-        // JSON data array
-        let results = result.data;
+        // Loop through each result item //
+        for (var i = 0; i < result.data.length; i++) {
+            // JSON data array
+            let results = result.data;
+                
+            // Create a div to hold the gif //
+            var gifContainer = $("<div class='gif'>");
             
-        // Create a div to hold the gif //
-        var gifContainer = $("<div class='gif'>");
-        
-        // Create an element to hold the image //
-        var gifImage = $("<img>");
+            // Create an element to hold the image //
+            var gifImage = $("<img>");
 
-        // Add still gif/image source 
-        gifImage.attr("src", results[i].images.fixed_height_still.url);
+            // Add still gif/image source 
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
 
-        // Add gif rating
-        // var ratingText = $("<p>").text("Caption: " + results[i].rating);
+            // Add gif rating
+            // var ratingText = $("<p>").text("Caption: " + results[i].rating);
 
-        // Add the gif to the view //
-        gifContainer.append(gifImage);
-        // gifContainer.append(ratingText);
+            // Add the gif to the view //
+            gifContainer.append(gifImage);
+            // gifContainer.append(ratingText);
 
-        // Putting the gifs at the beginning //
-        $("#display-gifs").prepend(gifContainer);
-    }
-});
+            // Putting the gifs at the beginning //
+            $("#display-gifs").prepend(gifContainer);
+        }
+    });
+}
 
 // Pause and play gifs
 $("#display-gifs").on("click", ".gif img", function() {
@@ -81,6 +83,8 @@ $("button#enter-value").click(function(event) {
     //Clear input field of text
     $("input").val("");
 });
+
+$(document).click(".gif-btn", displayGifs);
 
 // Call topicsButtons function to populate buttons
 topicsButtons();
